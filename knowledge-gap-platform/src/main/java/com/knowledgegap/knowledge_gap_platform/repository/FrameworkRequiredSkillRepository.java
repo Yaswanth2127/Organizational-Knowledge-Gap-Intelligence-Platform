1d259cbd5ac76ea3975eb89bc8e7fac5287dad3b
Yaswanth <patnanayaswanth0@gmail.com>
2026-07-04 21:58:52 +0530

    Added JPA repositories and initial service implementation

---
+++ knowledge-gap-platform/src/main/java/com/knowledgegap/knowledge_gap_platform/repository/FrameworkRequiredSkillRepository.java
@@ -0,0 +1,7 @@
+package com.knowledgegap.knowledge_gap_platform.repository;
+
+import com.knowledgegap.knowledge_gap_platform.entity.FrameworkRequiredSkill;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FrameworkRequiredSkillRepository extends JpaRepository<FrameworkRequiredSkill,Long> {
+}
