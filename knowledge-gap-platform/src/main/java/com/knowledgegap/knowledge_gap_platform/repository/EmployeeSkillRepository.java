c5f3517757e9eb6dda378ed2acce65639d67dcfb
MEGAVARSHINI2004 <megavarhsini3012@gmail.com>
2026-07-06 00:16:25 +0530

    Implemented EmployeeSkill CRUD API

--- knowledge-gap-platform/src/main/java/com/knowledgegap/knowledge_gap_platform/repository/EmployeeSkillRepository.java
+++ knowledge-gap-platform/src/main/java/com/knowledgegap/knowledge_gap_platform/repository/EmployeeSkillRepository.java
@@ -3,5 +3,6 @@ package com.knowledgegap.knowledge_gap_platform.repository;
 import com.knowledgegap.knowledge_gap_platform.entity.EmployeeSkill;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeSkillRepository extends JpaRepository<EmployeeSkill,Long> {
+public interface EmployeeSkillRepository extends JpaRepository<EmployeeSkill, Long> {
+
 }
