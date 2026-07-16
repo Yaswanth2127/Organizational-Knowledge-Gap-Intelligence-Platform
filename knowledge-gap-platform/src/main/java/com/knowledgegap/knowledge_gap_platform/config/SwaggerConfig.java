9b4f0d552d456fbd2c48b6a80e7035634f54e4a7
Megavarshini A <123164852+MEGAVARSHINI2004@users.noreply.github.com>
2026-07-16 20:18:37 +0530

    Update SwaggerConfig.java

--- knowledge-gap-platform/src/main/java/com/knowledgegap/knowledge_gap_platform/config/SwaggerConfig.java
+++ knowledge-gap-platform/src/main/java/com/knowledgegap/knowledge_gap_platform/config/SwaggerConfig.java
@@ -1,10 +1,12 @@
 package com.knowledgegap.knowledge_gap_platform.config;
 
-import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,22 +14,39 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI knowledgeGapOpenAPI() {
+    public OpenAPI knowledgeGapAPI() {
+
+        final String securitySchemeName = "bearerAuth";
 
         return new OpenAPI()
+
                 .info(new Info()
+
                         .title("Knowledge Gap Intelligence Platform API")
-                        .version("1.0")
+
                         .description("REST APIs for Organizational Knowledge Gap Intelligence Platform")
 
+                        .version("1.0")
+
                         .contact(new Contact()
-                                .name("Megavarshini")
+                                .name("Knowledge Gap Team")
                                 .email("knowledgegapintelligence@gmail.com"))
 
                         .license(new License()
-                                .name("Apache 2.0")))
+                                .name("MIT License")))
 
-                .externalDocs(new ExternalDocumentation()
-                        .description("Knowledge Gap Platform Documentation"));
+                .addSecurityItem(
+                        new SecurityRequirement().addList(securitySchemeName))
+
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                ));
     }
 }
