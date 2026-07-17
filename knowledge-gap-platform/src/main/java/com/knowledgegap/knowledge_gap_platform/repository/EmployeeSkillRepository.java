a387cd0d5a80ef135433fc32fc8a5e42e4560c68
Yaswanth <patnanayaswanth0@gmail.com>
2026-07-17 18:26:33 +0530

    refactor: optimize entity fetching with EntityGraph and disable Open Session in View

--- knowledge-gap-platform/src/main/java/com/knowledgegap/knowledge_gap_platform/repository/EmployeeSkillRepository.java
+++ knowledge-gap-platform/src/main/java/com/knowledgegap/knowledge_gap_platform/repository/EmployeeSkillRepository.java
@@ -1,11 +1,37 @@
 package com.knowledgegap.knowledge_gap_platform.repository;
 
 import com.knowledgegap.knowledge_gap_platform.entity.EmployeeSkill;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface EmployeeSkillRepository extends JpaRepository<EmployeeSkill, Long> {
+    @EntityGraph(attributePaths = {
+            "user",
+            "skill"
+            ,"user.department"
+            ,"user.jobRole"
+    })
     List<EmployeeSkill> findByUserId(Long userId);
+
+    @Override
+    @EntityGraph(attributePaths = {
+            "user",
+            "skill"
+            ,"user.department"
+            ,"user.jobRole"
+    })
+    List<EmployeeSkill>findAll();
+
+    @Override
+    @EntityGraph(attributePaths = {
+            "user",
+            "skill"
+            ,"user.department"
+            ,"user.jobRole"
+    })
+    Optional<EmployeeSkill> findById(Long id);
 
 }
