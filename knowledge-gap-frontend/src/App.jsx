dfa99d7a1caff955705f09cc7b47fa36be5a6765
Yaswanth <patnanayaswanth0@gmail.com>
2026-07-26 11:20:48 +0530

    feat: implement event-driven skill gap analysis and update frontend

--- knowledge-gap-frontend/src/App.jsx
+++ knowledge-gap-frontend/src/App.jsx
@@ -21,6 +21,7 @@ import CertificationManagement from "./pages/CertificationManagement";
 import CourseManagement from "./pages/CourseManagement";
 import FrameworkRequiredSkillManagement from "./pages/FrameworkRequiredSkillManagement";
 import SkillGapManagement from "./pages/SkillGapManagement";
+import EmployeeSkillGapAnalysis from "./pages/EmployeeSkillGapAnalysis";
 
 // Protected Route wrapper â€” token check karke hi andar jaane deta hai
 function ProtectedRoute({ children }) {
@@ -155,7 +156,16 @@ function App() {
               </ProtectedRoute>
           }
       />
+       <Route
+        path="/employee/skill-gap"
+        element={
+            <ProtectedRoute roles={["EMPLOYEE"]}>
+                <EmployeeSkillGapAnalysis />
+            </ProtectedRoute>
+        }
+    />
       </Routes>
+     
     </BrowserRouter>
   );
 }
